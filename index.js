81bcc1c40df9307ba83d5aa36876a178fd3b5f77
Roman M <rmarchen95@gmail.com>
2022-12-21 20:24:30 -0800

    finished assignment got code working fully

--- index.js
+++ index.js
@@ -1,103 +1,26 @@
 const inventory = newInventory()
 move(inventory).to(0, 0)
 
-// const character = newImage('assets/green-character.gif')
-// move(character).withArrowKeys(100, 250)
-
-
 const character = newImage('assets/green-character/static.gif')
-let direction = null;
-let x = 100;
-let y = 250;
-move(character).to(100, 250)
 
-function moveCharacter() {
+function handleDirectionChange(direction) {
+    if (direction === null) {
+        character.src = 'assets/green-character/static.gif'
+    }
     if (direction === 'west') {
-        x = x - 1
+        character.src = 'assets/green-character/west.gif'
     }
     if (direction === 'north') {
-        y = y + 1
+        character.src = 'assets/green-character/north.gif'
     }
     if (direction === 'east') {
-        x = x + 1
+        character.src = 'assets/green-character/east.gif'
     }
     if (direction === 'south') {
-        y = y - 1
+        character.src = 'assets/green-character/south.gif'
     }
-    character.style.left = x + 'px'
-    character.style.bottom = y + 'px'
 }
-setInterval(moveCharacter, 1)
-
-document.addEventListener('keydown', function (e) {
-    if (e.repeat) return;
-
-    if (e.key === 'ArrowLeft') {
-        direction = 'west'
-    }
-    if (e.key === 'ArrowUp') {
-        direction = 'north'
-    }
-    if (e.key === 'ArrowRight') {
-        direction = 'east'
-    }
-    if (e.key === 'ArrowDown') {
-        direction = 'south'
-    }
-})
-
-document.addEventListener('keyup', function (e) {
-    direction = null
-})
-
-function moveWithArrowKeys(left, bottom) {
-    let direction = null;
-    let x = left;
-    let y = bottom;
-
-    element.style.left = x + 'px'
-    element.style.bottom = y + 'px'
-
-    function moveCharacter() {
-        if (direction === 'west') {
-            x -= 1
-        }
-        if (direction === 'north') {
-            y += 1
-        }
-        if (direction === 'east') {
-            x += 1
-        }
-        if (direction === 'south') {
-            y -= 1
-        }
-        element.style.left = x + 'px'
-        element.style.bottom = y + 'px'
-    }
-
-    setInterval(moveCharacter, 1)
-
-    document.addEventListener('keydown', function (e) {
-        if (e.repeat) return;
-
-        if (e.key === 'ArrowLeft') {
-            direction = 'west'
-        }
-        if (e.key === 'ArrowUp') {
-            direction = 'north'
-        }
-        if (e.key === 'ArrowRight') {
-            direction = 'east'
-        }
-        if (e.key === 'ArrowDown') {
-            direction = 'south'
-        }
-    })
-
-    document.addEventListener('keyup', function (e) {
-        direction = null
-    })
-}
+move(character).withArrowKeys(100, 250, handleDirectionChange)
 
 
 
